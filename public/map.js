d4391a79fe3b7b1926c02d30827b60af7dce7cb0
formation <formation@ubuntu.ubuntu-domain>
2021-03-18 14:35:00 +0100

    Bouton pour récupérer les coordonnées depuis la carte

--- public/map.js
+++ public/map.js
@@ -13,8 +13,43 @@ var map = new ol.Map({
    })
  ],
  view: new ol.View({
-   center: ol.proj.fromLonLat([2.584, 48.841]),
+   center: ol.proj.fromLonLat([2.344, 48.86]),
    zoom: 12,
    maxZoom: 20,
  })
 });
+
+var formulaire = document.getElementById('form')
+
+
+function getInfoMap(){
+  /*
+  formulaire.longitude.value = 2.344
+  formulaire.latitude.value = 48.86
+  formulaire.radius.value = 0.03
+  */
+
+  let coords = ol.proj.toLonLat( map.getView().getCenter())
+  formulaire.longitude.value = coords[0]
+  formulaire.latitude.value = coords[1]
+  formulaire.radius.value = (map.getView().calculateExtent(map.getSize())[2]-map.getView().calculateExtent(map.getSize())[0])/200000
+
+  console.log( map.getView().calculateExtent(map.getSize()) )
+  console.log( (map.getView().calculateExtent(map.getSize())[2]-map.getView().calculateExtent(map.getSize())[0])/200000 )
+
+}
+
+var getInfoMapButton = document.getElementById('getInfoMap')
+getInfoMapButton.addEventListener('click', getInfoMap)
+
+function checkFunction(){
+  if (checkbox.checked) {
+    setTimeout(function () {
+    getInfoMap();
+    return(checkFunction());
+  }, 500);}
+}
+
+var checkbox = document.getElementById('myCheckbox')
+
+checkbox.addEventListener('change', checkFunction)
